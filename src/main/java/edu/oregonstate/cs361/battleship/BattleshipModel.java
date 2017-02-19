1a0c23b08cf43e5c7bada564a9c013c5e7f1cdc4
bolanosf <bolanosf@oregonstate.edu>
2017-02-19 01:50:44 -0800

    Add files via upload
    added randomizing ships

--- src/main/java/edu/oregonstate/cs361/battleship/BattleshipModel.java
+++ src/main/java/edu/oregonstate/cs361/battleship/BattleshipModel.java
@@ -1,5 +1,4 @@
 package edu.oregonstate.cs361.battleship;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -8,6 +7,8 @@ import java.util.Random;
  * Created by michaelhilton on 1/4/17.
  */
 public class BattleshipModel {
+
+    int[][] board = new int[10][10];
 
     private Ship aircraftCarrier = new Ship("AircraftCarrier",5, new Coordinate(0,0),new Coordinate(0,0));
     private Ship battleship = new Ship("Battleship",4, new Coordinate(0,0),new Coordinate(0,0));
@@ -27,8 +28,6 @@ public class BattleshipModel {
     private ArrayList<Coordinate> computerMisses;
 
     int scanResult = 0;
-
-
 
     public BattleshipModel() {
         playerHits = new ArrayList<>();
@@ -157,4 +156,134 @@ public class BattleshipModel {
     public int getScanResult() {
         return scanResult;
     }
+
+    public void  RandShips() {
+        int lengths[] = {2, 2, 3, 4, 5};
+
+        for (int k = 0; k < 5; k++) {
+
+            int xcord = 0;
+            int ycord = 0;
+            int orientation = 0;
+            int overlap = 0;
+            int counter = 0;
+            int size= lengths[k];
+            int[] StartCord=new int[2];
+            int[] EndCord=new int[2];
+
+            Random rand = new Random();
+            boolean crash = true;
+            boolean sameloc = true;
+
+            while (crash) {
+                overlap = 0;
+                if (counter == 0)
+                    orientation = rand.nextInt(1) + 1;
+                else if (counter == 2) {
+                    counter = 0;
+                    orientation = rand.nextInt(1) + 1;
+                    sameloc = true;
+                }
+                else
+                    orientation++;
+
+
+                while (sameloc) {
+                    xcord = rand.nextInt(9);
+                    ycord = rand.nextInt(9);
+
+                    if (board[xcord][ycord] == 0)
+                        sameloc = false;
+                }
+
+                //horizontal right orientation check
+                if (orientation %2 == 0) {
+                    if ((ycord + size) <= 9) {
+                        for (int i = ycord; i < (ycord + size); i++) {
+                            if (board[xcord][i] == 1)
+                                overlap = 1;
+                        }
+
+                        if (overlap == 0) {
+                            for (int i = ycord; i < (ycord + size); i++) {
+                                board[xcord][i] = 1;
+                                EndCord[0]=xcord;
+                                EndCord[1]=i;
+                            }
+                            crash = false;
+                        }
+                    }
+                }
+
+                //vertical down orientation check
+                if (orientation % 2 == 1) {
+                    if ((xcord + size) <= 9) {
+                        for (int i = xcord; i < (xcord + size); i++) {
+                            if (board[i][ycord] == 1)
+                                overlap = 1;
+                        }
+
+                        if (overlap == 0) {
+                            for (int i = xcord; i < (xcord + size); i++) {
+                                board[i][ycord] = 1;
+                                EndCord[0]=i;
+                                EndCord[1]=ycord;
+                            }
+                            crash = false;
+                        }
+                    }
+                }
+
+                counter++;
+                StartCord[0]=xcord;
+                StartCord[1]=ycord;
+            }
+
+
+            //records the starting and ending corddinates for each ship
+            if(k==0){
+                this.getShip("submarine").setLocation(new Coordinate(StartCord[0]+1,StartCord[1]+1),new Coordinate(EndCord[0]+1,EndCord[1]+1));
+                /*System.out.print(StartCord[0]+1);
+                System.out.print(StartCord[1]+1);
+                System.out.print("\n");
+                System.out.print(EndCord[0]+1);
+                System.out.print(EndCord[1]+1);
+                System.out.print("\n");
+                //SubmarineCordsStart=StartCord.clone();
+                //SubmarineCordsEnd=EndCord.clone();*/
+            }
+            else if(k==1){
+                this.getShip("destroyer").setLocation(new Coordinate(StartCord[0]+1,StartCord[1]+1),new Coordinate(EndCord[0]+1,EndCord[1]+1));
+            }
+            else if(k==2){
+                this.getShip("Cruiser").setLocation(new Coordinate(StartCord[0]+1,StartCord[1]+1),new Coordinate(EndCord[0]+1,EndCord[1]+1));
+            }
+            else if(k==3){
+                this.getShip("battleship").setLocation(new Coordinate(StartCord[0]+1,StartCord[1]+1),new Coordinate(EndCord[0]+1,EndCord[1]+1));
+            }
+            else if(k==4){
+                this.getShip("aircraftcarrier").setLocation(new Coordinate(StartCord[0]+1,StartCord[1]+1),new Coordinate(EndCord[0]+1,EndCord[1]+1));
+            }
+        }
+    }
+
+    public void printf() {
+        for (int x = 0; x < 10; x++) {
+            for (int y = 0; y < 10; y++) {
+                System.out.print(board[x][y]);
+            }
+            System.out.print("\n");
+        }
+        System.out.print(submarine);
+        System.out.print("\n");
+        System.out.print(destroyer);
+        System.out.print("\n");
+        System.out.print(cruiser);
+        System.out.print("\n");
+        System.out.print(battleship);
+        System.out.print("\n");
+        System.out.print(aircraftCarrier);
+        System.out.print("\n");
+    }
+
 }
